a20556f44dbe87535f1fc0b1f444592b0e24ccca
Chien-Yu Xiong <s-xiongch@bsd405.org>
2020-12-23 11:10:28 -0800

    Added Enemy Files

--- Periphery/Assets/Scripts/Enemy/EnemyBase.cs
+++ Periphery/Assets/Scripts/Enemy/EnemyBase.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBase : MonoBehaviour
+public abstract class EnemyBase : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
