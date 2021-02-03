41015d68b6c298c1adf0b0e13832c6fa57c37d17
Andrew Zhou <andrewzhou16@gmail.com>
2021-02-03 15:03:45 -0800

    Added Working Lance

--- Periphery/Assets/Scripts/Fighting/Lance.cs
+++ Periphery/Assets/Scripts/Fighting/Lance.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Lance : MonoBehaviour
+public class Lance : WeaponBase
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 localPosition;
+    private float joystickMag = 0f;
+    public override void InitWeapon()
     {
-        
+        //todo: make weapon point in the same direction as the previous one when weapon switched
+        Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), base.PlayerCollider);
+        transform.rotation = Quaternion.identity;
+        transform.localPosition = Vector3.zero;
+        localPosition = transform.localPosition;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void UpdateWeapon()
     {
-        
+        if (!base.JoystickIsPressed)
+        {
+            transform.localPosition = localPosition;
+            return;
+        }
+        Quaternion target = Quaternion.Euler(0, 0, Mathf.Rad2Deg * Mathf.Atan2(base.JoystickDirection.y, base.JoystickDirection.x) - 90);
+        gameObject.transform.rotation = Quaternion.Lerp(gameObject.transform.rotation, target, Time.fixedDeltaTime * 7);
+      
+        joystickMag = base.JoystickDirection.magnitude;       
+        transform.position = base.PlayerPos + (transform.up * joystickMag * WeaponBase.moveScale);
+        localPosition = transform.localPosition;
+
+    }
+
+    public override void StopWeapon()
+    {
+        Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), base.PlayerCollider, false); //undo ignore
     }
 }
