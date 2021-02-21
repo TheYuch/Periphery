1099c30fea59fa6dbc9ba5889c45f931f34f3f9e
Andrew Zhou <andrewzhou16@gmail.com>
2021-02-20 19:27:32 -0800

    Smooth Camera Follow and Temporary Standard Enemy

---
+++ Periphery/Assets/Scripts/Movement/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    Camera mainCam;
+
+    const float lerpSpeed = 5f;
+    private void Start()
+    {
+        mainCam = Camera.main;
+    }
+    void Update()
+    {
+        Vector3 camInitialPosition = mainCam.transform.position;
+        Vector3 camFinalPosition = new Vector3(this.transform.position.x, this.transform.position.y, mainCam.transform.position.z);
+        mainCam.transform.position = Vector3.Lerp(camInitialPosition, camFinalPosition, lerpSpeed * Time.deltaTime);
+    }
+}
